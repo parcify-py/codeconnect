a292d28787ebbef66b0def8d1901051b6e8ef575
PetrRyb <petr.rybin@infis.cz>
2025-05-07 13:18:00 +0200

    Add files via upload

---
+++ heslo.py
@@ -0,0 +1,7 @@
+import random
+maláPísmena = ["A", "B", "C", "D", "E"]
+str.lower
+maláPísmena[:10]
+randomchoice = random.choice(maláPísmena)
+v4 = randomchoice
+print(f"Vybral jsem písmeno {v4} na indexu {maláPísmena.index(randomchoice)}")
