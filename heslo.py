f4e6f7198d16d697fa9baade083383e668f60367
gosha32cm <alina.leshchenko@infis.cz>
2025-05-07 13:02:12 +0200

    Update heslo.py

--- heslo.py
+++ heslo.py
@@ -4,9 +4,3 @@ v5 = random.choice(smajlici)
 index = smajlici.index(v5)
 print(f"Vybral jsem smajlíka {v5} na indexu {index}")
 
-
-import random
-smajlici = ["😊", "😊", "😊", "😊", "😊", "😊", "😊", "😊"]
-index = random.randint(0, len(smajlici) - 1)
-v5 = smajlici[index]
-print(f"Vybral jsem smajlíka {v5} na indexu {index}")
