eea5d81d3830102de4657318b7ff690a486ee293
Maajer <martin.majer@infis.cz>
2025-05-07 13:17:26 +0200

    Update heslo.py

--- heslo.py
+++ heslo.py
@@ -1,13 +1,9 @@
 import random
 
-# Vytvoření pole se zvířaty
 zvirata = ["pes", "kočka", "myš", "kráva", "kůň"]
 
-# Výběr náhodného zvířete
 v7 = random.choice(zvirata)
 
-# Zjištění indexu vybraného zvířete
 index = zvirata.index(v7)
 
-# Výpis výsledku
 print("Vybral jsem zvíře \"" + v7 + "\" co se vybralo na indexu " + str(index))
