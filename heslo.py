cd4d0c6e01b6af35b3e700b583765725965a9e9f
wwickss <viktoriia.savchuk@infis.cz>
2025-05-07 13:01:56 +0200

    Add files via upload

---
+++ heslo.py
@@ -0,0 +1,11 @@
+import random
+
+colour = ["pink", "black", "orange", "green", "white"]
+nahodna_colour = random.choice(colour)
+print(f"I chose colour {nahodna_colour} that I chose from {colour.index(nahodna_colour)} index")
+
+
+
+
+
+
